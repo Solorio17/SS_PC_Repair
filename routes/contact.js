32396fc22db524ee81e79a18d69a2afe690f71cc
Solorio17 <jasonsoloriom@gmail.com>
2017-09-02 01:03:54 -0600

    Adding nodemailer module, using transport to connect to gmail account, andfinally creating a sort of schema for very basic design

--- routes/contact.js
+++ routes/contact.js
@@ -1,8 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require("nodemailer");
 
 router.get('/', function(req, res, next) {
   res.render('contact', { title: 'Contact' });
+});
+
+router.post("/send", function(req, res, next){
+    var transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth:{
+        user: "testingakount17@gmail.com",
+        pass: "testingakountpassword"
+      }
+    });
+
+    var mailOptions = {
+      from: '"Test Account" <testingakount17@gmail.com>',
+      to : 'testakkount217@gmail.com',
+      subject: "Hello from test account 1",
+      text: 'You have a submission from... Name: '+req.body.name+ ' Email: '+req.body.email+ ' Message: '+req.body.message
+    }
+    transporter.sendMail(mailOptions, function(err, info){
+      if(err){
+        console.log(err);
+      }else {
+        console.log("Message Sent: " + info.response);
+        res.redirect("/");
+      }
+    });
 });
 
 module.exports = router;
